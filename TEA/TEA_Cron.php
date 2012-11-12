c28a7ae608fab442997a580ecfa22432e3a581bb
barneycg <barney@mother>
2012-11-12 15:21:30 +0000

    Running code

--- TEA/TEA_Cron.php
+++ TEA/TEA_Cron.php
@@ -1,6 +1,9 @@
 <?php
+
 if (file_exists('../SSI.php') && !defined('SMF'))
+{
 	require_once('../SSI.php');
+}
 if (file_exists(dirname(dirname(__FILE__)) . '/SSI.php'))
 	require_once(dirname(dirname(__FILE__)) . '/SSI.php');
 
