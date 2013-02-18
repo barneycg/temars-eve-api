39a56386d742836e535e1bf2c237aa8112eb95b3
barneycg <barney@mother>
2013-02-18 19:58:27 +0000

    increasing rev number

--- TEA.php
+++ TEA.php
@@ -23,7 +23,7 @@ class TEA extends TEAC
 		$this -> smcFunc = &$smcFunc;
 		$this -> settings = &$settings;
 
-		$this -> version = "1.3.0 r172";
+		$this -> version = "1.3.0 r173";
 
 		$permissions["tea_view_own"] = 1;
 		$permissions["tea_view_any"] = 0;
